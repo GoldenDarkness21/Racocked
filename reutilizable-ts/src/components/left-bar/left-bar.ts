ae4fea14c39ea3040db109283d0eed6afa5bef9c
Brandon <martinezbran0221@gmail.com>
2024-09-30 09:39:13 -0500

    full main page develop

--- reutilizable-ts/src/components/left-bar/left-bar.ts
+++ reutilizable-ts/src/components/left-bar/left-bar.ts
@@ -56,6 +56,7 @@ class UserSidebar extends HTMLElement {
                                 <path d="M19.4 15a1.77 1.77 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.77 1.77 0 0 0-1.82-.33 1.77 1.77 0 0 0-1 1.6v.09a2 2 0 0 1-4 0v-.09a1.77 1.77 0 0 0-1-1.6 1.77 1.77 0 0 0-1.82.33l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06A1.77 1.77 0 0 0 5 15a1.77 1.77 0 0 0-1.6-1H3.31a2 2 0 0 1 0-4h.09a1.77 1.77 0 0 0 1.6-1 1.77 1.77 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06A1.77 1.77 0 0 0 9 5a1.77 1.77 0 0 0 1-1.6V3.31a2 2 0 0 1 4 0v.09a1.77 1.77 0 0 0 1 1.6 1.77 1.77 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06a1.77 1.77 0 0 0-.33 1.82v.09a1.77 1.77 0 0 0 1.6 1z"></path>
                             </svg>
                         </button>
+                        <div class="exit">
                         <button class="menu-item">
                             <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out">
                                 <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
@@ -63,6 +64,7 @@ class UserSidebar extends HTMLElement {
                                 <line x1="21" y1="12" x2="9" y2="12"></line>
                             </svg>
                         </button>
+                        </div>
                     </div>
                 </div>
             `;
