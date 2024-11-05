f8969bb02202c235d661e652d4b2d4249321af1a
sarism <saraisabelmayalasso@gmail.com>
2024-11-05 15:55:53 -0500

    fix(create post): corregir error en la visualización de la  barra de la izquierda

--- reutilizable-ts/src/screens/5createpost/createPost.ts
+++ reutilizable-ts/src/screens/5createpost/createPost.ts
@@ -3,219 +3,182 @@ import { addPost } from '../../utils/firebase';
 import { addObserver, appState, dispatch } from '../../store';
 import { getProductsAction } from '../../store/actions';
 import styles from './styles.css';
-import UserSidebar, { SidebarAttribute } from '../../components/left-bar/left-bar';
-
+import './path/to/LeftBar'; // Asegúrate de ajustar la ruta según la ubicación real de tu componente
 
 const post: Post = {
-	name: '',
-	ingredients: '',
-	preparation: '',
-	categorie:  '',
+    name: '',
+    ingredients: '',
+    preparation: '',
+    categorie:  '',
     time: '',
     difficulty: '',
-
 };
 
 class Createpost extends HTMLElement {
-	constructor() {
-		super();
-		this.attachShadow({ mode: 'open' });
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
 
-		const style = document.createElement('style');
-		style.textContent = styles; 
-		this.shadowRoot?.appendChild(style);
-		addObserver(this);
-	}
+        const style = document.createElement('style');
+        style.textContent = styles;
+        this.shadowRoot?.appendChild(style);
+        addObserver(this);
+    }
 
-	async connectedCallback() {
-		// if (appState.posts.length === 0 || appState == undefined) {
-		// 	const action = await getProductsAction();
-		// 	dispatch(action);
-		// } else {
-		// 	this.render();
-		// }
-		this.render();
-	}
+    async connectedCallback() {
+        this.render();
+    }
 
-	changeName(e: any) {
-		post.name = e.target.value;
-	}
+    changeName(e: any) {
+        post.name = e.target.value;
+    }
 
-	changeIngredients(e: any) {
-		post.ingredients = e.target.value;
-	}
+    changeIngredients(e: any) {
+        post.ingredients = e.target.value;
+    }
 
     changePreparation(e: any) {
-		post.preparation = e.target.value;
-	}
+        post.preparation = e.target.value;
+    }
 
     changeCategorie(e: any) {
-		post.categorie = e.target.value;
-	}
+        post.categorie = e.target.value;
+    }
 
     changeTime(e: any) {
-		post.time = e.target.value;
-	}
+        post.time = e.target.value;
+    }
 
     changeDifficulty(e: any) {
-		post.difficulty = e.target.value;
-	}
+        post.difficulty = e.target.value;
+    }
 
-	submitForm() {
-		addPost(post);
-	}
+    submitForm() {
+        addPost(post);
+    }
 
-	 render() {
-		if (this.shadowRoot) {
-			this.shadowRoot.innerHTML = ` 
-                <link rel="stylesheet" href="../src/styles.css">
-                <div id="main-container">
-                    <div id="sidebar">
-                        <user-sidebar ${SidebarAttribute.profilePicture}></user-sidebar>
-                    </div>
-					` 
+    render() {
+        if (this.shadowRoot) {
+            // Instancia de LeftBar
+            const leftBar = this.ownerDocument.createElement('left-bar');
+            leftBar.classList.add('left-bar'); // Añade clase si quieres estilizar LeftBar
 
+            // Crear secciones
             const section1 = this.ownerDocument.createElement('section');
-			section1.classList.add('principal-section');
-			const section2 = this.ownerDocument.createElement('section');
-			section2.classList.add('image-section');
-			const section3 = this.ownerDocument.createElement('section'); 
-			section3.classList.add('form-section');
-			const section4 = this.ownerDocument.createElement('section'); 
-			section4.classList.add('category-section');
-			
-			
+            section1.classList.add('principal-section');
+            section1.appendChild(leftBar); // Añadir LeftBar aquí
 
+            const section2 = this.ownerDocument.createElement('section');
+            section2.classList.add('image-section');
 
-            
+            const section3 = this.ownerDocument.createElement('section');
+            section3.classList.add('form-section');
 
+            const section4 = this.ownerDocument.createElement('section');
+            section4.classList.add('category-section');
 
             // Título del formulario
-		    const title = this.ownerDocument.createElement('h1');
-		    title.innerText = 'New Post!';
-		    section3.appendChild(title);
-		    title.classList.add('title');
+            const title = this.ownerDocument.createElement('h1');
+            title.innerText = 'New Post!';
+            section3.appendChild(title);
+            title.classList.add('title');
 
-            //campo del nombre
+            // Campo del nombre
             const nameLabel = this.ownerDocument.createElement('label');
-		    nameLabel.innerText = 'Recipe name';
-            nameLabel.htmlFor = 'recipe name'
-            section3.appendChild(nameLabel)
-		    nameLabel.classList.add('label');
+            nameLabel.innerText = 'Recipe name';
+            nameLabel.htmlFor = 'recipe name';
+            section3.appendChild(nameLabel);
+            nameLabel.classList.add('label');
 
-			const pName = this.ownerDocument.createElement('input');
-			pName.addEventListener('change', this.changeName);
-			section3.appendChild(pName);
-			pName.classList.add('name');
+            const pName = this.ownerDocument.createElement('input');
+            pName.addEventListener('change', this.changeName.bind(this));
+            section3.appendChild(pName);
+            pName.classList.add('name');
 
-
-            //campo de los ingredientes
+            // Campo de los ingredientes
             const ingredientsLabel = this.ownerDocument.createElement('label');
-		    ingredientsLabel.innerText = 'Ingredients';
-            ingredientsLabel.htmlFor = 'ingredients'
-            section3.appendChild(ingredientsLabel)
-		    ingredientsLabel.classList.add('label');
+            ingredientsLabel.innerText = 'Ingredients';
+            ingredientsLabel.htmlFor = 'ingredients';
+            section3.appendChild(ingredientsLabel);
+            ingredientsLabel.classList.add('label');
 
-			const pIngredients = this.ownerDocument.createElement('input');
-			pIngredients.addEventListener('change', this.changeIngredients);
-			section3.appendChild(pIngredients);
+            const pIngredients = this.ownerDocument.createElement('input');
+            pIngredients.addEventListener('change', this.changeIngredients.bind(this));
+            section3.appendChild(pIngredients);
             pIngredients.classList.add('ingredients');
 
-
-
-            //campo de preparation
+            // Campo de preparación
             const preparationLabel = this.ownerDocument.createElement('label');
-		    preparationLabel.innerText = 'Preparation';
-            preparationLabel.htmlFor = 'preparation'
-            section3.appendChild(preparationLabel)
-		    preparationLabel.classList.add('label');
+            preparationLabel.innerText = 'Preparation';
+            preparationLabel.htmlFor = 'preparation';
+            section3.appendChild(preparationLabel);
+            preparationLabel.classList.add('label');
 
-			const pPreparation = this.ownerDocument.createElement('input');
-			pPreparation.addEventListener('change', this.changePreparation);
-			section3.appendChild(pPreparation);
+            const pPreparation = this.ownerDocument.createElement('input');
+            pPreparation.addEventListener('change', this.changePreparation.bind(this));
+            section3.appendChild(pPreparation);
             pPreparation.classList.add('preparation');
 
+            // Campo de la categoría
+            const categoryContainer = this.ownerDocument.createElement('div');
+            const categoryLabel = this.ownerDocument.createElement('label');
+            categoryLabel.innerText = 'Category';
+            categoryLabel.htmlFor = 'category';
+            categoryContainer.appendChild(categoryLabel);
+            categoryLabel.classList.add('label');
 
+            const pCategory = this.ownerDocument.createElement('input');
+            pCategory.id = 'category';
+            pCategory.addEventListener('change', this.changeCategorie.bind(this));
+            categoryContainer.appendChild(pCategory);
+            pCategory.classList.add('category');
 
-			// Campo de la categoría
-			const categoryContainer = this.ownerDocument.createElement('div');
-			const categoryLabel = this.ownerDocument.createElement('label');
-			categoryLabel.innerText = 'Category';
-			categoryLabel.htmlFor = 'category';
-			categoryContainer.appendChild(categoryLabel);
-			categoryLabel.classList.add('label');
+            section4.appendChild(categoryContainer);
 
-			const pCategory = this.ownerDocument.createElement('input');
-			pCategory.id = 'category';
-			pCategory.addEventListener('change', this.changeCategorie);
-			categoryContainer.appendChild(pCategory);
-			pCategory.classList.add('category');
+            // Campo del tiempo
+            const timeContainer = this.ownerDocument.createElement('div');
+            const timeLabel = this.ownerDocument.createElement('label');
+            timeLabel.innerText = 'Time';
+            timeLabel.htmlFor = 'time';
+            timeContainer.appendChild(timeLabel);
+            timeLabel.classList.add('label');
 
-			section4.appendChild(categoryContainer);
+            const pTime = this.ownerDocument.createElement('input');
+            pTime.id = 'time';
+            pTime.addEventListener('change', this.changeTime.bind(this));
+            timeContainer.appendChild(pTime);
+            pTime.classList.add('time');
 
-			// Campo del tiempo
-			const timeContainer = this.ownerDocument.createElement('div');
-			const timeLabel = this.ownerDocument.createElement('label');
-			timeLabel.innerText = 'Time';
-			timeLabel.htmlFor = 'time';
-			timeContainer.appendChild(timeLabel);
-			timeLabel.classList.add('label');
+            section4.appendChild(timeContainer);
 
-			const pTime = this.ownerDocument.createElement('input');
-			pTime.addEventListener('change', this.changeTime);
-			timeContainer.appendChild(pTime);
-			pTime.classList.add('time');
+            // Campo de dificultad
+            const difficultyContainer = this.ownerDocument.createElement('div');
+            const difficultyLabel = this.ownerDocument.createElement('label');
+            difficultyLabel.innerText = 'Difficulty';
+            difficultyLabel.htmlFor = 'difficulty';
+            difficultyContainer.appendChild(difficultyLabel);
+            difficultyLabel.classList.add('label');
 
-			section4.appendChild(timeContainer);
+            const pDifficulty = this.ownerDocument.createElement('input');
+            pDifficulty.id = 'difficulty';
+            pDifficulty.addEventListener('change', this.changeDifficulty.bind(this));
+            difficultyContainer.appendChild(pDifficulty);
+            pDifficulty.classList.add('difficulty');
 
-			// Campo de la dificultad
-			const difficultyContainer = this.ownerDocument.createElement('div');
-			const difficultyLabel = this.ownerDocument.createElement('label');
-			difficultyLabel.innerText = 'Difficulty';
-			difficultyLabel.htmlFor = 'difficulty';
-			difficultyContainer.appendChild(difficultyLabel);
-			difficultyLabel.classList.add('label');
+            section4.appendChild(difficultyContainer);
 
-			const pDifficulty = this.ownerDocument.createElement('input');
-			pDifficulty.id = 'difficulty';
-			pDifficulty.addEventListener('change', this.changeDifficulty);
-			difficultyContainer.appendChild(pDifficulty);
-			pDifficulty.classList.add('difficulty');
+            // Botón de submit
+            const submitButton = this.ownerDocument.createElement('button');
+            submitButton.innerText = 'Submit';
+            submitButton.addEventListener('click', this.submitForm.bind(this));
+            section3.appendChild(submitButton);
+            submitButton.classList.add('submit-button');
 
-			section4.appendChild(difficultyContainer);
-
-
-
-
-            //boton para publicar un post
-			const publish = this.ownerDocument.createElement('button');
-			publish.innerText = 'Publish';
-			publish.addEventListener('click', this.submitForm);
-			
-
-			// Agregar la segunda y tercera sección dentro de section 1 y la 1 al shadowroot
-			section1.appendChild(section2);
-			section1.appendChild(section3);			
-			section3.appendChild(section4);
-			section3.appendChild(publish);
-			this.shadowRoot.appendChild(section1);
-			
-
-
-
-			// appState.posts?.forEach((post: any) => {
-			// 	const container = this.ownerDocument.createElement('section');
-
-			// 	const name = this.ownerDocument.createElement('h3');
-			// 	name.innerText = post.hola;
-			// 	container.appendChild(name);
-
-
-			// 	this.shadowRoot?.appendChild(container);
-			// });
-		}
-	}
+            // Agregar todas las secciones al Shadow DOM
+            this.shadowRoot.append(section1, section2, section3, section4);
+        }
+    }
 }
 
-customElements.define('app-createpost', Createpost);
+customElements.define('create-post', Createpost);
 export default Createpost;
