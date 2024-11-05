8f16a6818a912b38908a007e267784f8923b99d0
sarism <saraisabelmayalasso@gmail.com>
2024-11-05 10:30:28 -0500

    refactor(left-bar):mejorar logica de navegacion

--- reutilizable-ts/src/components/left-bar/left-bar.ts
+++ reutilizable-ts/src/components/left-bar/left-bar.ts
@@ -1,3 +1,6 @@
+import { logOut } from "../../utils/firebase";
+
+
 export enum SidebarAttribute {
     'profilePicture' = 'profilePicture', 
 }
@@ -19,8 +22,24 @@ class UserSidebar extends HTMLElement {
     }
 
     connectedCallback() {
-        this.render();  
+        this.render();
+        this.addEventListeners(); 
     }
+    addEventListeners() {
+        const logoutButton = this.shadowRoot?.querySelector('#logout');
+        if (logoutButton) {
+            logoutButton.addEventListener('click', () => {
+                console.log('logout clickeado');
+                logOut().then(() => {
+                    console.log('Logout exitoso');
+                    // Puedes redirigir al usuario o realizar otras acciones aquí
+                }).catch((error) => {
+                    console.error('Error al hacer logout:', error);
+                });
+            });
+        }
+    }
+
 
     render() {
         if (this.shadowRoot) {
@@ -56,8 +75,9 @@ class UserSidebar extends HTMLElement {
                                 <path d="M19.4 15a1.77 1.77 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.77 1.77 0 0 0-1.82-.33 1.77 1.77 0 0 0-1 1.6v.09a2 2 0 0 1-4 0v-.09a1.77 1.77 0 0 0-1-1.6 1.77 1.77 0 0 0-1.82.33l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06A1.77 1.77 0 0 0 5 15a1.77 1.77 0 0 0-1.6-1H3.31a2 2 0 0 1 0-4h.09a1.77 1.77 0 0 0 1.6-1 1.77 1.77 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06A1.77 1.77 0 0 0 9 5a1.77 1.77 0 0 0 1-1.6V3.31a2 2 0 0 1 4 0v.09a1.77 1.77 0 0 0 1 1.6 1.77 1.77 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06a1.77 1.77 0 0 0-.33 1.82v.09a1.77 1.77 0 0 0 1.6 1z"></path>
                             </svg>
                         </button>
+                        <button id ="logout" class="logout-btn">Log out</button>
                         <div class="exit">
-                        <button class="menu-item">
+                        <button id ="logout" class="menu-item">
                             <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out">
                                 <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
                                 <polyline points="16 17 21 12 16 7"></polyline>
