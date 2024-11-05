bc297f8907c5cbf6e75dd1eb7bd7f6d6474b2dc6
sarism <saraisabelmayalasso@gmail.com>
2024-11-05 10:58:31 -0500

    feat(left-bar): agregar icono de redireccion

--- reutilizable-ts/src/components/left-bar/left-bar.ts
+++ reutilizable-ts/src/components/left-bar/left-bar.ts
@@ -85,12 +85,13 @@ class UserSidebar extends HTMLElement {
                                 <circle cx="12" cy="7" r="4"></circle>
                             </svg>
                         </button>
-                        <button class="menu-item">
-                            <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings">
-                                <circle cx="12" cy="12" r="3"></circle>
-                                <path d="M19.4 15a1.77 1.77 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.77 1.77 0 0 0-1.82-.33 1.77 1.77 0 0 0-1 1.6v.09a2 2 0 0 1-4 0v-.09a1.77 1.77 0 0 0-1-1.6 1.77 1.77 0 0 0-1.82.33l-.06.06a2 2 0 1 1-2.83-2.83l.06-.06A1.77 1.77 0 0 0 5 15a1.77 1.77 0 0 0-1.6-1H3.31a2 2 0 0 1 0-4h.09a1.77 1.77 0 0 0 1.6-1 1.77 1.77 0 0 0-.33-1.82l-.06-.06a2 2 0 1 1 2.83-2.83l.06.06A1.77 1.77 0 0 0 9 5a1.77 1.77 0 0 0 1-1.6V3.31a2 2 0 0 1 4 0v.09a1.77 1.77 0 0 0 1 1.6 1.77 1.77 0 0 0 1.82-.33l.06-.06a2 2 0 1 1 2.83 2.83l-.06.06a1.77 1.77 0 0 0-.33 1.82v.09a1.77 1.77 0 0 0 1.6 1z"></path>
-                            </svg>
-                        </button>
+                        <button id="logout" class="logout-btn">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M10 17l5-5-5-5"></path>
+        <path d="M21 12H9"></path>
+        <path d="M14 7V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2v-2"></path>
+    </svg>
+</button>
                         <button id ="logout" class="logout-btn">Log out</button>
                         <div class="exit">
                         <button id ="logout" class="menu-item">
