61e04e4959eb5cfaf8fc1d421bcb56f225e4aa3e
sarism <saraisabelmayalasso@gmail.com>
2024-11-06 15:13:20 -0500

    refactor(post): agregar post a la dashboard

---
+++ reutilizable-ts/src/components/card-list/cardList.ts
@@ -0,0 +1,113 @@
+// import { appState, dispatch } from "../../store/index";
+// import { Post } from "../../types/post";
+// import Post, { Attribute } from "../card/card";
+
+
+// class TaskList extends HTMLElement {
+//     tasks: Task[] = [];
+
+//     constructor() {
+//         super();
+//         this.attachShadow({ mode: 'open' });
+//     }
+
+//     connectedCallback() {
+//         this.render();
+//         this.tasks = appState.tasks
+
+//         const taskListContainer = this.shadowRoot?.querySelector('.task-list');
+
+//         this.tasks.forEach(task => {
+//             // Verifica que 'uid', 'utitle', y 'description' existan
+//             if (task.uid && task.utitle && task.description) {
+//                 const taskItem = this.ownerDocument.createElement('task-item') as TaskItem;
+//                 taskItem.setAttribute(Attribute.uid, task.uid.toString());
+//                 taskItem.setAttribute(Attribute.utitle, task.utitle);
+//                 taskItem.setAttribute(Attribute.description, task.description);
+//                 taskItem.setAttribute(Attribute.completedTask, task.completedTask ? 'true' : 'false');
+
+
+//                 taskListContainer?.appendChild(taskItem);
+//             } else {
+//                 console.error('Task is missing required attributes:', task);
+//             }
+//         });
+                
+//     }
+
+
+//     render() {
+//         if (!this.shadowRoot) return;
+
+//         this.shadowRoot.innerHTML = `
+//             <style>
+//                 .task-list {
+//                     display: flex;
+//                     flex-direction: column;
+//                     gap: 10px;
+//                 }
+//             </style>
+//             <h2>Tasks</h2>
+//             <div class="task-list">
+//             </div>
+//         `;
+        
+//     }
+// }
+
+// customElements.define('task-list', TaskList);
+// export default TaskList
+
+
+
+import { Post } from '../../types/post';
+import { addPost } from '../../utils/firebase';
+import { addObserver, appState, dispatch } from '../../store';
+import { getProductsAction } from '../../store/actions';
+
+const post: Post = {
+    name: "",
+    ingredients: "",
+    preparation: "",
+    categorie: "",
+    time: "",
+    difficulty: "",
+};
+
+class CardList extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+		addObserver(this);
+	}
+
+	async connectedCallback() {
+		if (appState.posts.length === 0) {
+			const action = await getProductsAction();
+			dispatch(action);
+		} else {
+			this.render();
+		}
+	}
+
+
+	async render() {
+		if (this.shadowRoot) {
+			
+			appState.posts?.forEach((post: any) => {
+				const container = this.ownerDocument.createElement('section');
+
+				const name = this.ownerDocument.createElement('h3');
+				name.innerText = post.name;
+				container.appendChild(name);
+
+
+
+				this.shadowRoot?.appendChild(container);
+			});
+		}
+	}
+}
+
+customElements.define('card-list', CardList);
+export default CardList;
