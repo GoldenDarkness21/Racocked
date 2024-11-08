b048966d5726e1b2411f8d7d9c745acfa014863f
Brandon <martinezbran0221@gmail.com>
2024-11-08 00:51:21 -0500

    fetch (PostpopUp): better popup structure

--- reutilizable-ts/src/components/PostPopup/PostPopup.ts
+++ reutilizable-ts/src/components/PostPopup/PostPopup.ts
@@ -33,11 +33,11 @@ class PostPopup extends HTMLElement {
                         <button id="close-button">X</button>
                         <h2>${this.post.name}</h2>
                         <p class="ingredients">Ingredients: <P>${this.post.ingredients}</p>
-                        <p class="preparation">Preparation:<P> ${this.post.preparation}</p>
+                        <p class="preparation">Preparation:<P>${this.post.preparation}</p>
                         <div class="tags-container">
                         <p class="categorie">${this.post.categorie}</p>
                         <p class="time">${this.post.time}</p>
-                        <p class="difficulty"> ${this.post.difficulty}</p>
+                        <p class="difficulty">${this.post.difficulty}</p>
                         </div>
 
                     </div>
