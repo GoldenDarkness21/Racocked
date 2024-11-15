06850657f3f20f1d6873874574cfa3a377f36028
Brandon <martinezbran0221@gmail.com>
2024-11-15 15:59:50 -0500

    feat (add profile screen) : page working

--- reutilizable-ts/src/indexAbuelo.ts
+++ reutilizable-ts/src/indexAbuelo.ts
@@ -2,6 +2,7 @@ import './screens/1register/1register';
 import './screens/3dashboard/dashboard';
 import './screens/2login/2login';
 import './screens/5createpost/createPost';
+import './screens/6profile/profile';
 import { addObserver, appState } from './store';
 import { Screens } from './types/navegation';
 
--- reutilizable-ts/src/screens/6profile/profile.ts
+++ reutilizable-ts/src/screens/6profile/profile.ts
@@ -1,8 +1,4 @@
 import recipes from '../../data/data';
-import stories from '../../data/dataProfile'; 
-import Post, { Attribute } from '../../components/card/card';
-import Storie, { StorieAttribute } from '../../components/profileStorie/storie';
-import SearchBar, { SearchAttribute } from '../../components/search-bar/searchBar';
 import UserSidebar, { SidebarAttribute } from '../../components/left-bar/left-bar';
 import BottomNavbar, { NavbarAttribute } from '../../components/bottomBar/BottomNavbar'; // Importar el BottomNavbar
 import { appState, dispatch } from '../../store';
@@ -13,19 +9,11 @@ import { Screens } from '../../types/store';
 
 
 class Profile extends HTMLElement {
-    recipesList: Post[] = [];
-    storiesList: HTMLElement[] = []; 
 
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
 
-
-        stories.forEach(story => {
-            const storyElement = this.ownerDocument.createElement("profile-storie") as Storie; 
-            storyElement.setAttribute(StorieAttribute.photo, story.photo);
-            this.storiesList.push(storyElement);
-        });
     }
 
     async connectedCallback() {
@@ -40,13 +28,7 @@ class Profile extends HTMLElement {
                     <div id="sidebar">
                         <user-sidebar ${SidebarAttribute.profilePicture}></user-sidebar>
                     </div>
-                    <div id="content">
-                        <div id="arriba">
-                            <div id="story-container"></div>
-                            <div id="derecha">
-                                <search-bar ${SearchAttribute.placeholder}="Search recipe..."></search-bar>
-                            </div>
-                        </div>
+                    <div id="content"></div>
                     </div>
                     
                     <bottom-navbar ${NavbarAttribute.activeIcon}="home"></bottom-navbar>
@@ -58,5 +40,5 @@ class Profile extends HTMLElement {
         }
     }
 
-customElements.define('profile', Profile);
+customElements.define('app-profile', Profile);
 export default Profile;
